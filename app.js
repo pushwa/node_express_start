eec6d7c68514ae3c7bf3ed03a5685ceb16cb4239
Vidar Korneliussen <vidar@pushwagner.net>
2021-09-21 09:29:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('view engine', 'ejs');
 
